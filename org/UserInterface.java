760d1ad454d1987de46905d060b2ae4853a79249
Om Shastri <64175925+Om-Shastri@users.noreply.github.com>
2024-06-18 20:20:56 -0700

    Update UserInterface.java

--- org/UserInterface.java
+++ org/UserInterface.java
@@ -29,101 +29,30 @@ public class UserInterface {
 				System.out.println("Enter the fund number to see more information.");
 			}
 			System.out.println("Enter 0 to create a new fund");
+			System.out.println("Enter -1 to list all contributions");
 
-			while(true){
-				String userString = in.nextLine();
-				if (userString.equals("q") || userString.equals("quit")){
-					System.out.println("Good-bye!");
-					System.exit(0);
-				}
-				try{
-					int option = Integer.parseInt(userString);
-					if (option == 0) {
-						createFund();
-						break;
-					}
-					else if (option>0 && option <= org.getFunds().size()){
-						displayFund(option);
-						break;
-	
-					}
-					else {
-						System.out.println("Please enter a number of a fund, 0 to create a fund, or q to quit.");
-	
-					}
-				}
-				catch (NumberFormatException e){
-					System.out.println("Please enter a number or q to quit. ");
-				}
-	
-
-
+			int option = in.nextInt();
+			in.nextLine();
+			if (option == 0) {
+				createFund();
+			} else if (option == -1) {
+				listAllContributions();
+			} else {
+				displayFund(option);
 			}
-
-
-
-
-
-			// int option = in.nextInt();
-			// in.nextLine();
-			// if (option == 0) {
-			// 	createFund(); 
-			// }
-			// else {
-			// 	displayFund(option);
-			// }
-		}			
-			
+		}
 	}
 
 	public void createFund() {
 		System.out.print("Enter the fund name: ");
-		String name;
-		while (true){
-			name = in.nextLine().trim();
-			if (name.equals("")){
-				System.out.println("The fund name can't be blank. Please enter the fund name. ");
-			}
-			else{
-				break;
-			}
-		}
-		
+		String name = in.nextLine().trim();
+
 		System.out.print("Enter the fund description: ");
-		String description;
-		while (true){
-			description = in.nextLine().trim();
-			if (description.equals("")){
-				System.out.println("The description can't be blank. Please enter the description. ");
-			}
-			else{
-				break;
-			}
-		}
+		String description = in.nextLine().trim();
 
 		System.out.print("Enter the fund target: ");
-		long target;
-		while(true){
-			String userString = in.nextLine();
-			try{
-				target = Integer.parseInt(userString);
-				if (target < 0){
-					System.out.println("The target must be positive.");
-				}
-				else {
-					break;
-				}
-
-			}
-			catch (NumberFormatException e){
-				System.out.println("Please enter an integer.");
-			} 			
-
-		}
-
-
-		// long target = in.nextInt();
-		// in.nextLine();
+		long target = in.nextInt();
+		in.nextLine();
 
 		Fund fund = dataManager.createFund(org.getId(), name, description, target);
 		org.getFunds().add(fund);
@@ -156,6 +85,16 @@ public class UserInterface {
 		in.nextLine();
 	}
 
+	public void listAllContributions() {
+		List<Donation> allDonations = dataManager.getAllContributions(org);
+		System.out.println("\n\nAll Contributions:");
+		for (Donation donation : allDonations) {
+			System.out.println("Fund: " + donation.getFundId() + ", Amount: $" + donation.getAmount() + ", Date: " + formatDate(donation.getDate()));
+		}
+		System.out.println("Press the Enter key to go back to the main menu");
+		in.nextLine();
+	}
+
 	private String formatDate(String dateString) {
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
@@ -168,33 +107,19 @@ public class UserInterface {
 		}
 	}
 
-	
-	
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		DataManager ds = new DataManager(new WebClient("localhost", 3001));
 
 		String login = args[0];
 		String password = args[1];
-		//System.out.println(login + " " + password);
-		try {
-			Organization org = ds.attemptLogin(login, password);
-			
-			if (org == null) {
-				System.out.println("Login failed.");
-			}
-			else {
-	
-				UserInterface ui = new UserInterface(ds, org);
-			
-				ui.start();
-			
-			}
-		}
-		catch(IllegalStateException e){
-			System.out.println("Error in Communicating with server");	
+
+		Organization org = ds.attemptLogin(login, password);
+
+		if (org == null) {
+			System.out.println("Login failed.");
+		} else {
+			UserInterface ui = new UserInterface(ds, org);
+			ui.start();
 		}
 	}
 }
